5aa67932abd7380bc19ecd5168430600e0eaeab6
samar-vafaei <samarvafai@gmail.com>
2024-11-08 19:44:56 +0330

    change psql queries

--- pgsql.sql
+++ pgsql.sql
@@ -1,69 +1,32 @@
-CREATE FUNCTION NotifyOnDataChange()
+CREATE FUNCTION UpdateCreditOfSubscriber()
   RETURNS trigger  
 AS $BODY$ 
-DECLARE 
-  data JSON;
-  notification JSON;
 BEGIN
-
-  IF (TG_OP = 'DELETE') THEN
-    data = row_to_json(OLD);
-  ELSE
-    data = row_to_json(NEW);
-  END IF;
- 
-  notification = json_build_object(
-            'table',TG_TABLE_NAME,
-            'action', TG_OP, 
-            'data', data);  
-            
-    -- note that channel name MUST be lowercase, otherwise pg_notify() won't work
-    PERFORM pg_notify('tbl2', notification::TEXT);
+	WITH temp AS (
+		SELECT new_table_acc.from_uri AS username, (subscriber.credit-new_table_acc.duration*billing.call_rates) AS credit
+		FROM new_table_acc 
+		JOIN billing 
+		ON country_code=LEFT(to_uri,7)
+		JOIN subscriber 
+		ON from_uri=username)	
+	UPDATE subscriber SET subscriber.credit=temp.credit FROM temp WHERE subscriber.username = temp.username;
+	
   RETURN NEW;
 END;
 $BODY$ LANGUAGE plpgsql;
 
-CREATE TRIGGER OnDataChange_INS_subscriber
-  AFTER INSERT ON subscriber
-  REFERENCING NEW TABLE AS new_table_subscriber
-  FOR EACH STATEMENT EXECUTE FUNCTION NotifyOnDataChange();
 
-CREATE TRIGGER OnDataChange_UPD_subscriber
-  AFTER UPDATE ON subscriber
-  REFERENCING OLD TABLE AS old_table_subscriber NEW TABLE AS new_table_subsciber
-  FOR EACH STATEMENT EXECUTE FUNCTION NotifyOnDataChange();
+CREATE TRIGGER OnDataChange_INS_acc
+  AFTER INSERT ON acc
+  REFERENCING NEW TABLE AS new_table_acc
+  FOR EACH STATEMENT EXECUTE FUNCTION UpdateCreditOfSubscriber();  
+  
+  
+  
+ 
+	
+	
+	
+		
 
-CREATE TRIGGER OnDataChange_DEL_subscriber
-  AFTER DELETE ON subscriber
-  REFERENCING OLD TABLE AS old_table_subscriber
-  FOR EACH STATEMENT EXECUTE FUNCTION NotifyOnDataChange();
 
-CREATE TRIGGER OnDataChange_INS_billing
-  AFTER INSERT ON billing
-  REFERENCING NEW TABLE AS new_table_billing
-  FOR EACH STATEMENT EXECUTE FUNCTION NotifyOnDataChange();
-
-CREATE TRIGGER OnDataChange_UPD_billing
-  AFTER UPDATE ON billing
-  REFERENCING OLD TABLE AS old_table_billing NEW TABLE AS new_table_billing
-  FOR EACH STATEMENT EXECUTE FUNCTION NotifyOnDataChange();
-
-CREATE TRIGGER OnDataChange_DEL_billing
-  AFTER DELETE ON billing
-  REFERENCING OLD TABLE AS old_table_billing
-  FOR EACH STATEMENT EXECUTE FUNCTION NotifyOnDataChange();
-
-CREATE TRIGGER OnDataChange_INS_cdr
-  AFTER INSERT ON cdr
-  REFERENCING NEW TABLE AS new_table_cdr
-  FOR EACH STATEMENT EXECUTE FUNCTION NotifyOnDataChange();
-
-CREATE TRIGGER OnDataChange_UPD_cdr
-  AFTER UPDATE ON cdr
-  REFERENCING OLD TABLE AS old_table_cdr NEW TABLE AS new_table_cdr
-  FOR EACH STATEMENT EXECUTE FUNCTION NotifyOnDataChange();
-
-CREATE TRIGGER OnDataChange_DEL_cdr
-  AFTER DELETE ON cdr
-  REFERENCING OLD TABLE AS old_table_cdr
-  FOR EACH STATEMENT EXECUTE FUNCTION NotifyOnDataChange();
